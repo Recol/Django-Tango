2eab0829a307c7f5362a24ba700a7b16136e49dd
Deco <13683901+Recol@users.noreply.github.com>
2021-05-22 23:52:18 +0100

    Implement Rango pic CH4

--- main/files/tango_with_django_project/settings.py
+++ main/files/tango_with_django_project/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'tango_with_django_project/templates')
-print(TEMPLATE_DIR, BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
