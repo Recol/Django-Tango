4c950af243533b881eb4fb76269281d9459a0b3b
Deco <13683901+Recol@users.noreply.github.com>
2021-05-23 02:46:41 +0100

    Revert "CH6 Final Wrap Up + Merger"
    This reverts commit eef3c3722471e9c0491fefd1f64e3750af80930b.

--- main/files/rango/views.py
+++ main/files/rango/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
-from rango.models import Category, Page
+from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to the {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-    context_dict['pages'] = page_list
-
-    # Return a rendered response to send to the client. We make use of the shortcut function to
-    # make our lives easier. Note that the first parameter is the template we wish to use.
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -23,10 +17,8 @@ def about(request):
 
     return render(request, 'rango/about.html', context=context_dict)
 
-
 def show_category(request, category_name_slug):
     context_dict = {}
-
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
